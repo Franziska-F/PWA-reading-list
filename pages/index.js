071266b33be9396589457f5401f86b645724008b
Franziska-F <f.fouchs@gmail.com>
2022-08-07 21:23:57 +0200

    Fetching books vom google API, message when no cover

--- pages/index.js
+++ pages/index.js
@@ -3,6 +3,21 @@ import { useState } from 'react';
 
 export default function Home() {
   const [title, setTitle] = useState('');
+  const [result, setResult] = useState([]);
+
+  function handleSearch() {
+    fetch(`https://www.googleapis.com/books/v1/volumes?q=intitle:${title}`)
+      .then((res) =>
+        res.json().then((response) => {
+          const bookInfo = response.items;
+
+          setResult(bookInfo);
+        }),
+      )
+      .catch(() => {
+        console.log('error');
+      });
+  }
   return (
     <>
       <h1 className="text-center my-4">Was willst du als nächstes lesen?</h1>
@@ -15,9 +30,49 @@ export default function Home() {
           }}
         />
         <div className="w-1/4">
-          <button className="bg-yellow-300 w-full rounded text-black my-4 ">
+          <button
+            className="bg-yellow-300 w-full rounded text-black my-4 "
+            onClick={() => handleSearch()}
+          >
             Suchen
           </button>{' '}
+        </div>
+
+        <div className="grid grid-cols-1 md:grid-cols-3 gap-x-4 gap-y-8 px-8 ">
+          {result.map((item) => {
+            return (
+              <div
+                className="flex justify-center items-center py-4"
+                key={item.id}
+              >
+                <a href={`/books/${item.id}`}>
+                  <div className="flex justify-center items-center">
+                    {item.volumeInfo.imageLinks !== undefined ? (
+                      <img
+                        className="rounded"
+                        src={
+                          item.volumeInfo.imageLinks !== undefined
+                            ? item.volumeInfo.imageLinks.thumbnail
+                            : ''
+                        }
+                        alt="bookcover"
+                      />
+                    ) : (
+                      <p>No cover available!</p>
+                    )}
+                  </div>
+                  <div className="my-4 text-center">
+                    <p>{item.volumeInfo.title}</p>
+                    <p>
+                      {item.volumeInfo.authors
+                        ? item.volumeInfo.authors[0]
+                        : 'Unknowen'}
+                    </p>
+                  </div>
+                </a>
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
