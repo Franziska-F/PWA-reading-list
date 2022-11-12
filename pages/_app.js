7a844265c74a2c710a91b468c499d5f1d818f840
Franziska-F <f.fouchs@gmail.com>
2022-11-12 21:48:17 +0100

    Add  icons for pwa

--- pages/_app.js
+++ pages/_app.js
@@ -1,4 +1,5 @@
 import '../styles/globals.css';
+import Head from 'next/head';
 import { useCallback, useEffect, useState } from 'react';
 import Layout from '../components/Layout';
 
@@ -20,7 +21,23 @@ function MyApp({ Component, pageProps }) {
 
   return (
     <Layout bookList={bookList} setBookList={setBookList}>
-      {' '}
+      <Head>
+        <>
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/img/pwa-icon-128x128.png"
+            type="image/png"
+            sizes="128x128"
+          />
+          <link
+            href="/img/pwa-icon-144x144.png"
+            type="image/png"
+            sizes="144x144"
+          />
+
+          <link rel="apple-touch-icon" href="/apple-icon.png" />
+        </>
+      </Head>{' '}
       <Component
         bookList={bookList}
         setBookList={setBookList}
